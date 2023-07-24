79b564c85ddbca0e7378d988490a0643956d6cc6
litovskii-s <113451236+litovskii-s@users.noreply.github.com>
2023-07-24 12:40:40 +0300

    update

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,31 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+
+  function openModal() {
+    refs.modal.classList.remove("is-hidden");
+    document.body.style.overflow = "hidden";
+    document.addEventListener("keydown", handleEscapeKey);
+  }
+
+  function closeModal() {
+    refs.modal.classList.add("is-hidden");
+    document.body.style.overflow = "auto";
+    document.removeEventListener("keydown", handleEscapeKey);
+  }
+
+  function handleEscapeKey(event) {
+    if (event.key === "Escape") {
+      // Закрываем модальное окно при нажатии клавиши Escape
+      closeModal();
     }
-  })();
+  }
+})();
+
+
